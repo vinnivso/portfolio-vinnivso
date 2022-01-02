56d21461886ac8a0e0993ba5a06e6fb33ca42585
vinnivso <vinnivsob@gmail.com>
2022-01-02 12:23:54 -0300

    Feat: Qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,16 +51,16 @@ skillsHeader.forEach(element => {
 const tabs = document.querySelectorAll("[data-target]"),
   tabContents = document.querySelectorAll("[data-content]")
 
-tabs.forEach(tab => {
+tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target)
 
-    tabContents.forEach(tabContent => {
+    tabContents.forEach((tabContent) => {
       tabContent.classList.remove("qualification__active")
     })
     target.classList.add("qualification__active")
 
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.classList.remove("qualification__active")
     })
     tab.classList.add("qualification__active")
