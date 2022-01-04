066da90a1d6204e51615e0812c7cf97306b49eed
vinnivso <vinnivsob@gmail.com>
2022-01-04 20:12:57 -0300

    Feat: Header Section - Navbar & Links to Sections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
 
+/* ----------------
+ Toggle Navbar
+------------------- */
+const navToggler = document.querySelector(".nav-toggler")
+  navToggler.addEventListener("click", () => {
+    hideSection()
+    toggleNavbar()
+    document.body.classList.toggle("hide-scrolling")
+  })
 
-/* ---------------- About Tabs  ---------------- */
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out")
+}
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active")
+}
+
+
+/* ----------------
+ Active Section
+------------------- */
+document.addEventListener("click", (e) => {
+  if(e.target.classList.contains("link-item") && e.target.hash !== "") {
+    //activate the overlay to prevent multiple clicks
+    document.querySelector(".overlay").classList.add("active")
+    navToggler.classList.add("hide")
+    if(e.target.classList.contains("nav-item")) {
+      toggleNavbar()
+    } else {
+      hideSection()
+      document.body.classList.add("hide-scrolling")
+    }
+    setTimeout(() => {
+      document.querySelector("section.active").classList.remove("active", "fade-out")
+      document.querySelector(e.target.hash).classList.add("active")
+      window.scrollTo(0,0)
+      document.body.classList.remove("hide-scrolling")
+      navToggler.classList.remove("hide")
+    document.querySelector(".overlay").classList.remove("active")
+
+    }, 500)
+  }
+})
+
+
+
+/* ----------------
+ About Tabs
+------------------- */
 const tabsContainer = document.querySelector(".about-tabs"),
   aboutSection = document.querySelector(".about-section")
 
@@ -16,7 +63,9 @@ tabsContainer.addEventListener("click", (e) => {
 })
 
 
-/* ---------------- Portfolio Item Details Popup  ---------------- */
+/* ----------------
+ Portfolio Item Details Popup
+------------------- */
 document.addEventListener("click", (e) => {
   if(e.target.classList.contains("view-project-btn")) {
     togglePortfolioPopup()
